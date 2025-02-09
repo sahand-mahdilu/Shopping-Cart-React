83c0cf5a1a76a87f2179f018e9393a88fd8b7e13
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-09 19:00:40 +0330

    feat : header content created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './Components/Header'
 
 function App() {
 
   return (
     <>
-      <div className='bg-red-500'>hello</div>
+    <Header/>
+      
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,17 @@
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="Header_container h-[180px]">
+        
+
+        <ul className="flex text-white ">
+            <li className="p-2 cursor-pointer hover:bg-blue-500 font-bold ">HOME</li>
+            <li className="p-2 cursor-pointer hover:bg-blue-500 font-bold ">MENU</li>
+            <li className="p-2 cursor-pointer hover:bg-blue-500 font-bold ">ABOUT US</li>
+            <li className="p-2 cursor-pointer hover:bg-blue-500 font-bold ">STORE</li>
+        </ul>
+        
+        <h1 className="text-center mt-6 text-6xl  text-white font-bold">Galexy Shop</h1>
+    </div>
   )
 }
