551338344eae6e802abfa4520f09b330f457d70d
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-09 19:36:09 +0330

    feat ; cart container div added

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -18,7 +18,24 @@ export default function Shop() {
 
         </div>
 
-       
+        <section className="cart_section mt-8">
+            <h2 className="text-center text-5xl font-bold">
+                    cart
+            </h2>
+
+            <div className="mt-4 cart_row flex items-center justify-evenly text-[18px] font-bold text-b">
+                <span>Item</span>
+                <span>price</span>
+                <span>doing</span>
+
+            </div>
+            <hr  />
+            <div className="cart_container">
+
+               
+            </div>
+           
+        </section>
     </div>
   )
 }
